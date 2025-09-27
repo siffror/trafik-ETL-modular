285efd4a9b3c68990af32a598533d8cfbc5c7e5f
Zakaria <zakariyae.mokhtari@gmail.com>
2025-09-27 14:18:04 +0200

    Update config.py

--- src/trv/config.py
+++ src/trv/config.py
@@ -11,11 +11,11 @@ try:
     import streamlit as st
     TRV_API_KEY = st.secrets.get("TRV_API_KEY", os.getenv("TRV_API_KEY", ""))
     SLACK_WEBHOOK_URL = st.secrets.get("SLACK_WEBHOOK_URL", os.getenv("SLACK_WEBHOOK_URL", ""))
-    TRV_BASE_URL = st.secrets.get("TRV_BASE_URL", os.getenv("TRV_BASE_URL", "https://api.trafikverket.se"))
+    TRV_BASE_URL = st.secrets.get("TRV_BASE_URL", os.getenv("TRV_BASE_URL", "https://api.trafikinfo.trafikverket.se/v2/data.xml"))
 except ModuleNotFoundError:
     TRV_API_KEY = os.getenv("TRV_API_KEY", "")
     SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL", "")
-    TRV_BASE_URL = os.getenv("TRV_BASE_URL", "https://api.trafikverket.se")
+    TRV_BASE_URL = os.getenv("TRV_BASE_URL", "https://api.trafikinfo.trafikverket.se/v2/data.xml")
 
 # Default config
 DEFAULT_DAYS_BACK = int(os.getenv("DEFAULT_DAYS_BACK", "1"))
